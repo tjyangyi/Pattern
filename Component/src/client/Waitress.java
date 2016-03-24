a256b6eecfa60322247ddbf18ea82ce6679c8201
yangyi <yangyi@dvt.dvt.com>
2016-03-24 16:37:13 +0800

    遍历去掉syso
    Signed-off-by: yangyi <yangyi@dvt.dvt.com>

--- Component/src/client/Waitress.java
+++ Component/src/client/Waitress.java
@@ -17,15 +17,10 @@ public class Waitress {
 
 	public void printVegetarianMenu() {
 		Iterator<MenuComponent> iterator = this.allMenus.createIterator();
-		System.out.println("  ");
-		System.out.println("VEGETARIAN MENU");
-		System.out.println("------------");
 		while (iterator.hasNext()) {
 			MenuComponent menuComponent = iterator.next();
 			try {
-				//if (menuComponent.isVegetarian()) {
 					menuComponent.print();
-				//}
 			} catch (UnsupportedOperationException e) {
 			}
 		}
--- Component/src/composite/CompositeIterator.java
+++ Component/src/composite/CompositeIterator.java
@@ -10,69 +10,37 @@ public class CompositeIterator implements Iterator<MenuComponent> {
 
 	public CompositeIterator(Iterator<MenuComponent> iterator) {
 		this.stack.push(iterator);
-		//System.out.println(iterator + " is pushed into stack: " + this.stack);
+		// System.out.println(iterator + " is pushed into stack: " +
+		// this.stack);
 	}
 
 	@Override
 	public boolean hasNext() {
-	//	System.out.println("---------------------------");
-	//	System.out.println("进入hasNext方法:" + this.toString());
-	//	System.out.println("hasNext方法中:" + this.stack + "is empty?"
-	//			+ this.stack.empty());
 		if (this.stack.empty()) {
-		//	System.out.println("离开hasNext方法,离开值为:" + false);
-	//		System.out.println("---------------------------");
-	//		System.out.println();
 			return false;
 		}
 		Iterator<MenuComponent> iterator = this.stack.peek();
-	//	System.out.println("hasNext方法中:iterator = " + iterator);
 		if (iterator.hasNext()) {
-		//	System.out.println("hasNext方法中:iterator = " + iterator
-			//		+ " hasNext = true");
-	//		System.out.println("离开hasNext方法,离开值为:" + true);
-	//		System.out.println("---------------------------");
-//			System.out.println();
 			return true;
 		} else {
-	//		System.out.println("hasNext方法中:iterator = " + iterator
-	//				+ " hasNext = false");
 			this.stack.pop();
-	//		System.out.println("hasNext方法中:" + this.stack + " pop");
 			boolean has = this.hasNext();
-	//		System.out.println("离开hasNext方法,离开值为:" + has);
-	//		System.out.println("---------------------------");
-		//	System.out.println();
 			return has;
 		}
 	}
 
 	@Override
 	public MenuComponent next() {
-	//	System.out.println("---------------------------");
-	//	System.out.println("进入next方法:" + this.toString());
 		if (!this.hasNext()) {
-	//		System.out.println("离开next方法,离开值为:" + null);
-	//		System.out.println("---------------------------");
-	//		System.out.println();
 			return null;
 		}
 		Iterator<MenuComponent> iterator = this.stack.peek();
-	//	System.out.println("next方法中: iterator = " + iterator);
 		MenuComponent menuComponent = iterator.next();
-	//	System.out.println("next方法中: menuComponent = " + menuComponent);
-	//	System.out.println("next方法中: menuComponent is Menu ? "
-	//			+ (menuComponent instanceof Menu));
 		if (menuComponent instanceof Menu) {
-			Iterator<MenuComponent> compositeIterator = menuComponent.createIterator();
-	//		System.out.println("next方法中:CompositeIterator被Push进入stack:"
-				//	+ " menuComponent = " + compositeIterator
-			//		+ "  stack = " + this.stack);
+			Iterator<MenuComponent> compositeIterator = menuComponent
+					.createIterator();
 			this.stack.push(compositeIterator);
 		}
-	//	System.out.println("离开next方法,离开值为:" + menuComponent);
-	//	System.out.println("---------------------------");
-	//	System.out.println();
 		return menuComponent;
 	}
 
