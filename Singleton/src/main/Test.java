35d0079e3c33a5e10bd3994ae779e992fa4cd41d
yangyi <yangyi@dvt.dvt.com>
2016-03-31 08:50:05 +0800

    枚举单例

---
+++ Singleton/src/main/Test.java
@@ -0,0 +1,12 @@
+package main;
+
+import singleton.EnumSingleton;
+
+public class Test {
+
+	public static void main(String[] args) {
+		EnumSingleton.instance.method1();
+
+	}
+
+}
--- Singleton/src/singleton/Singleton.java
+++ Singleton/src/singleton/Singleton.java
@@ -16,4 +16,6 @@ public class Singleton {
 		}
 		return uniqueInstance;
 	}
+	
 }
+
